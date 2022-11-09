3829ba6133e751ae04044e361a8070cf3b48f3c7
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2022-11-09 13:04:01 +0200

    Encapsulated script in a function to import to src

--- data_generator.py
+++ data_generator.py
@@ -5,17 +5,18 @@ from faker.providers import internet
 
 faker = Faker()
 
-df = pd.DataFrame(data = {})
-df.Name = "attendees.csv"
-df.to_csv(df.Name, sep=',',index=False)
-id_prefixes = ['L-', 'W', 'HW-', 'SW-']
-names = []
-emails = []
-ids = []
+def generate_dummy_data():
+	df = pd.DataFrame(data = {})
+	df.Name = "attendees.csv"
+	df.to_csv(df.Name, sep=',',index=False)
+	id_prefixes = ['L-', 'W', 'HW-', 'SW-']
+	names = []
+	emails = []
+	ids = []
 
-n = eval(input("Number of rows? \n"))
+	n = eval(input("Number of rows? \n"))
 
-for _ in range(n):
+	for _ in range(n):
 		name = faker.name()
 		id = f'{random.choice(id_prefixes)}{faker.random_number(digits = 5)}'
 		first_name = name.split()[0].lower()
@@ -26,9 +27,10 @@ for _ in range(n):
 		emails.append(email)
 		ids.append(id)
 
-df['Name'] = names
-df['Email'] = emails
-df['ID'] = ids
+	df['Name'] = names
+	df['Email'] = emails
+	df['ID'] = ids
 
-df.to_csv("./attendees.csv")
-print(n, ' rows generated in ', df.Name)
+	df.to_csv("./attendees.csv")
+	print(n, ' rows generated in ', df.Name)
+	return df
